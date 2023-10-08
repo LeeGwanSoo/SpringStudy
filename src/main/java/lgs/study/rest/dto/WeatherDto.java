df796a0b9ae5427323cb63edd78930cdafaab432
GwanSoo <nicelgs1@naver.com>
2023-10-08 19:22:45 +0900

    feat : OpenApi 날씨 호출 결과 수정

--- src/main/java/lgs/study/rest/dto/WeatherDto.java
+++ src/main/java/lgs/study/rest/dto/WeatherDto.java
@@ -2,9 +2,13 @@ package lgs.study.rest.dto;
 
 import lombok.*;
 
+import java.util.List;
+
 @Data
 public class WeatherDto {
     String temperature;
     String wind;
     String description;
+
+    List<ForecastDto> forecast;
 }
